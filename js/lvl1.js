9b4e869fb9cb400feb0cc280d3612e61cbbba81b
NachoRomero <nacho2001nqn@hotmail.com>
2019-10-08 17:43:52 -0500

    juego 1

---
+++ js/lvl1.js
@@ -0,0 +1,206 @@
+// Configuración del juego.
+// Estos númmeros representan los campos. 
+let gameData = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,7,2,2,2,2,2,2,2,2,2,2,2,2,2,2,6,1],
+  [1,2,1,2,1,2,1,1,2,2,1,1,2,1,2,1,2,1],
+  [1,2,1,2,1,2,1,2,2,2,2,1,2,1,2,1,2,1],
+  [1,2,1,2,1,2,2,2,2,2,2,2,2,1,2,1,2,1],
+  [1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1],
+  [1,2,1,2,1,2,1,2,1,1,2,1,2,1,2,1,2,1],
+  [1,2,1,2,1,2,2,2,8,2,2,2,2,1,2,1,2,1],
+  [1,2,1,2,1,2,1,2,2,2,2,1,2,1,2,1,2,1],
+  [1,2,1,2,1,2,1,1,2,2,1,1,2,1,2,1,2,1],
+  [1,4,2,2,2,2,2,2,2,2,2,2,2,2,2,2,5,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+];
+
+// La pared es representada por el número 1.
+// Una moneda sería el numero dos , espacios vacíos se representan con un 3,
+// y el pacman es el número 8.
+
+
+// en este codigo se aclara el campo que es representado por cada número.
+const WALL   = 1;
+const COIN   = 2;
+const GROUND   = 3;
+const GHOSTA   = 4;
+const GHOSTB   = 5;
+const GHOSTC   = 6;
+const GHOSTD   = 7;
+const PACMAN   = 8;
+
+
+// Utilizaremos el identificador "map" para referirnos al mapa del juego.
+let map;
+
+// Con este código sabremos por donde está llendo pacman.
+// Esto se logra a través de un par de cordenadas.
+let pacman = {
+  x: 8,
+  y: 7,
+  direction: 'right'
+};
+
+// Creación de las funciones del mapa.
+
+function createTiles(data) {
+
+  
+  let tilesArray = [];
+
+  
+  for (let row of data) {
+
+    
+    for (let col of row) {
+
+     
+      let tile = document.createElement('div');
+
+    
+      tile.classList.add('tile');
+
+      
+      
+      if (col === WALL) {
+        tile.classList.add('wall');
+
+      } else if (col === COIN) {
+        tile.classList.add('coin');
+
+      } else if (col === GROUND) {
+        tile.classList.add('ground');
+
+      } else if (col === PACMAN) {
+        tile.classList.add('pacman');
+
+      } else if (col === GHOSTA){
+        tile.classList.add('ghosta');
+      
+      } else if (col === GHOSTB){
+        tile.classList.add('ghostb');
+      
+      } else if (col === GHOSTC){
+        tile.classList.add('ghostc');
+
+      } else if (col === GHOSTD){
+        tile.classList.add('ghostd');
+      
+
+        
+        tile.classList.add(pacman.direction);
+
+      }
+
+    
+      tilesArray.push(tile);
+    }
+
+  
+    let brTile = document.createElement('br');
+ 
+    tilesArray.push(brTile);
+  }
+  return tilesArray;
+}
+
+// Esta función agrega elementos del mapa,
+// y los añade a la página.
+function drawMap() {
+  map = document.createElement('div');
+
+  let tiles = createTiles(gameData);
+  for (let tile of tiles) {
+    map.appendChild(tile);
+  }
+
+  document.body.appendChild(map);
+}
+
+// Esta función borra los elementos del mapa desde la página.
+function eraseMap() {
+  document.body.removeChild(map);
+}
+
+// Funciones de movimiento.
+
+function moveDown() {
+  pacman.direction = 'down';
+  if (gameData[pacman.y+1][pacman.x] !== WALL) {
+    gameData[pacman.y][pacman.x] = GROUND;
+    pacman.y = pacman.y + 1 ;
+    gameData[pacman.y][pacman.x] = PACMAN;
+  }
+}
+
+function moveUp() {
+  pacman.direction = 'up';
+  if (gameData[pacman.y-1][pacman.x] !== WALL) {
+    gameData[pacman.y][pacman.x] = GROUND;
+    pacman.y = pacman.y - 1;
+    gameData[pacman.y][pacman.x] = PACMAN;
+  }
+}
+
+function moveLeft() {
+  pacman.direction = 'left';
+  if (gameData[pacman.y][pacman.x-1] !== WALL) {
+    gameData[pacman.y][pacman.x] = GROUND;
+    pacman.x = pacman.x - 1 ;
+    gameData[pacman.y][pacman.x] = PACMAN;
+  }
+}
+
+function moveRight() {
+  pacman.direction = 'right';
+  if (gameData[pacman.y][pacman.x+1] !== WALL) {
+    gameData[pacman.y][pacman.x] = GROUND;
+    pacman.x = pacman.x + 1 ;
+    gameData[pacman.y][pacman.x] = PACMAN;
+  }
+}
+
+// Esta función establece un listener para toda la página.
+// Cuando el usuario preciona una tecla se ejecuta una función
+function setupKeyboardControls() {
+  document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 37) {         
+      moveLeft();
+    } else if (e.keyCode === 38) {  
+      moveUp();
+
+    } else if (e.keyCode === 39){   
+      moveRight();
+
+    } else if (e.keyCode === 40){   
+      moveDown();
+    
+    } else if (e.keyCode === 87) {  
+      moveUp();
+
+    }if (e.keyCode === 65) {         
+      moveLeft();
+
+    } else if (e.keyCode === 68){   
+      moveRight();
+
+    } else if (e.keyCode === 83){   
+      moveDown();
+    }
+    // Luego de cada moviemiento el mapa se borra y rehace.
+    eraseMap();
+    drawMap();
+  });
+}
+
+// Configurución principal del juego.
+
+function main() {
+  drawMap();
+  setupKeyboardControls();
+}
+
+// Luego de definir todas las funciones se iniciara el juego.
+main();
+ 
---
+++ js/lvl2.js
@@ -0,0 +1,196 @@
+// Configuración del juego.
+// Estos númmeros representan los campos. 
+let gameData = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,1,2,2,2,2,2,1,2,2,2,2,2,1,1],
+  [1,2,2,1,1,1,2,1,2,1,1,1,2,2,1],
+  [1,2,1,1,2,2,2,2,2,2,2,1,1,2,1],
+  [1,2,1,2,2,1,1,3,1,1,2,2,1,2,1],
+  [1,2,1,2,1,4,3,3,3,8,1,2,1,2,1],
+  [1,2,2,2,1,3,3,3,3,3,1,2,2,2,1],
+  [1,1,1,2,1,3,3,3,3,3,1,2,1,1,1],
+  [1,2,2,2,1,3,3,3,3,3,1,2,2,2,1],
+  [1,2,1,2,1,3,6,3,7,3,1,2,1,2,1],
+  [1,2,1,2,2,1,1,3,1,1,2,2,1,2,1],
+  [1,2,1,1,2,2,2,5,2,2,2,1,1,2,1],
+  [1,2,2,1,1,1,2,1,2,1,1,1,2,2,1],
+  [1,1,2,2,2,2,2,1,2,2,2,2,2,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+];
+
+// La pared es representada por el número 1.
+// Una moneda sería el numero dos , espacios vacíos se representan con un 3,
+// y el pacman es el número 5.
+
+
+// en este codigo se aclara el campo que es representado por cada número.
+const WALL   = 1;
+const COIN   = 2;
+const GROUND   = 3;
+const GHOSTA   = 4;
+const GHOSTB   = 6;
+const GHOSTC   = 7;
+const GHOSTD   = 8;
+const PACMAN   = 5;
+
+
+// Utilizaremos el identificador "map" para referirnos al mapa del juego.
+let map;
+
+// Con este código sabremos por donde está llendo pacman.
+// Esto se logra a través de un par de cordenadas.
+let pacman = {
+  x: 7,
+  y: 11,
+  direction: 'right'
+};
+
+// Creación de las funciones del mapa.
+
+function createTiles(data) {
+
+  
+  let tilesArray = [];
+
+  
+  for (let row of data) {
+
+    
+    for (let col of row) {
+
+     
+      let tile = document.createElement('div');
+
+    
+      tile.classList.add('tile');
+
+      
+      
+      if (col === WALL) {
+        tile.classList.add('wall');
+
+      } else if (col === COIN) {
+        tile.classList.add('coin');
+
+      } else if (col === GROUND) {
+        tile.classList.add('ground');
+
+      } else if (col === PACMAN) {
+        tile.classList.add('pacman');
+
+      } else if (col === GHOSTA){
+        tile.classList.add('ghosta');
+      
+      } else if (col === GHOSTB){
+        tile.classList.add('ghostb');
+      
+      } else if (col === GHOSTC){
+        tile.classList.add('ghostc');
+
+      } else if (col === GHOSTD){
+        tile.classList.add('ghostd');
+        
+        tile.classList.add(pacman.direction);
+
+      }
+
+    
+      tilesArray.push(tile);
+    }
+
+  
+    let brTile = document.createElement('br');
+ 
+    tilesArray.push(brTile);
+  }
+  return tilesArray;
+}
+
+// Esta función agrega elementos del mapa,
+// y los añade a la página.
+function drawMap() {
+  map = document.createElement('div');
+
+  let tiles = createTiles(gameData);
+  for (let tile of tiles) {
+    map.appendChild(tile);
+  }
+
+  document.body.appendChild(map);
+}
+
+// Esta función borra los elementos del mapa desde la página.
+function eraseMap() {
+  document.body.removeChild(map);
+}
+
+// Funciones de movimiento.
+
+function moveDown() {
+  pacman.direction = 'down';
+  if (gameData[pacman.y+1][pacman.x] !== WALL) {
+    gameData[pacman.y][pacman.x] = GROUND;
+    pacman.y = pacman.y + 1 ;
+    gameData[pacman.y][pacman.x] = PACMAN;
+  }
+}
+
+function moveUp() {
+  pacman.direction = 'up';
+  if (gameData[pacman.y-1][pacman.x] !== WALL) {
+    gameData[pacman.y][pacman.x] = GROUND;
+    pacman.y = pacman.y - 1;
+    gameData[pacman.y][pacman.x] = PACMAN;
+  }
+}
+
+function moveLeft() {
+  pacman.direction = 'left';
+  if (gameData[pacman.y][pacman.x-1] !== WALL) {
+    gameData[pacman.y][pacman.x] = GROUND;
+    pacman.x = pacman.x - 1 ;
+    gameData[pacman.y][pacman.x] = PACMAN;
+  }
+}
+
+function moveRight() {
+  pacman.direction = 'right';
+  if (gameData[pacman.y][pacman.x+1] !== WALL) {
+    gameData[pacman.y][pacman.x] = GROUND;
+    pacman.x = pacman.x + 1 ;
+    gameData[pacman.y][pacman.x] = PACMAN;
+  }
+}
+
+// Esta función establece un listener para toda la página.
+// Cuando el usuario preciona una tecla se ejecuta una función
+function setupKeyboardControls() {
+  document.addEventListener('keydown', function (e) {
+    if (e.keyCode === 37) {         
+      moveLeft();
+
+    } else if (e.keyCode === 38) {  
+      moveUp();
+
+    } else if (e.keyCode === 39){   
+      moveRight();
+
+    } else if (e.keyCode === 40){   
+      moveDown();
+    }
+
+    // Luego de cada moviemiento el mapa se borra y rehace.
+    eraseMap();
+    drawMap();
+  });
+}
+
+// Configurución principal del juego.
+
+function main() {
+  drawMap();
+  setupKeyboardControls();
+}
+
+// Luego de definir todas las funciones se iniciara el juego.
+main();
